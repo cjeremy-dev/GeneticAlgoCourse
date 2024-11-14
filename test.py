d438483d651cff1efeb962a588ab68696ec4aeaf
Cao Zhiming <cao_zhiming2019@163.com>
2024-11-14 17:32:38 +0800

    chore: 删除无用文件
    - 移除 recv.txt 文件
    - 删除 test.py 和其他测试脚本
    - 清理项目中的冗余代码和文件

--- test.py
+++
@@ -1,6 +0,0 @@
-# Generate a code which draws the graph of the function f(x) = e ** x +2x  + 3 using matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
-x = np.linspace(-10, 10, 100)
-y = np.exp(x) + 2*x + 3
-plt.plot(x, y)
