7648b2f8a4a33980bdb6af9a6ba60bba35ba689a
cjeremy-dev <cao_zhiming2019@163.com>
2024-11-21 17:50:28 +0800

    feat(knapsack): 使用遗传算法解决背包问题
    - 更新了背包问题的代码实现，使用遗传算法进行求解
    - 添加了选择、交叉和变异函数，实现了完整的遗传算法流程
    - 优化了适应度计算方法，确保只计算有效解
    - 增加了世界记录读取和更新功能，用于保存最优解
    - 修改了.gitignore，更新了文件结构

---
+++ knapsack_data_random_generator.py
@@ -0,0 +1,12 @@
+import random
+
+ITEMS_NUM = 100
+
+weights = []
+values = []
+for i in range(ITEMS_NUM):
+    weights.append(random.uniform(0.1, 10))
+    values.append(random.randint(1, 70))
+
+print(weights)
+print(values)
